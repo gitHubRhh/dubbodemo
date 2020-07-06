b540a667b1bb2bf3bc68b0b1ecd47f1125f74b03
ruihaohao <rhh0809@163.com>
2020-07-06 17:27:46 +0800

    提交代码

---
+++ dubbo-demo-api/src/main/java/com/hippopotamus/dubbo/demo/DemoService.java
@@ -0,0 +1,9 @@
+package com.hippopotamus.dubbo.demo;
+
+/**
+ * @Author: HAOHAORUI
+ * @Date: Create in 4:15 PM 2020/7/6
+ * @Email: rhh0809@163.com
+ */
+public interface DemoService {
+}
---
+++ dubbo-demo-provider/src/main/java/com/hippopotamus/dubbo/demo/provider/DemoServiceImpl.java
@@ -0,0 +1,11 @@
+package com.hippopotamus.dubbo.demo.provider;
+
+import com.hippopotamus.dubbo.demo.DemoService;
+
+/**
+ * @Author: HAOHAORUI
+ * @Date: Create in 4:47 PM 2020/7/6
+ * @Email: rhh0809@163.com
+ */
+public class DemoServiceImpl implements DemoService {
+}
